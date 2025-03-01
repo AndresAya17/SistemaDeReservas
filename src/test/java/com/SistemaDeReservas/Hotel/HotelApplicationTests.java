a1df88d2990850f207627d57f08fd0b3dd3d45bd
AndresAya17 <38768365+AndresAya17@users.noreply.github.com>
2025-03-01 08:48:51 -0500

    Creacion proyecto raiz Sistema de reservas

---
+++ src/test/java/com/SistemaDeReservas/Hotel/HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SistemaDeReservas.Hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
